304d2ab42c24123da59882c412d73a6942fe5b88
Dada Khalandar <kerry.blig12@gmail.com>
2023-12-31 00:25:17 +0800

    fixed the input field, added props and no results found

--- components/search-input-box/search-input-box.tsx
+++ components/search-input-box/search-input-box.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import Image from 'next/image';
 import searchIcon from '@/public/assets/icon-search.svg';
 
-export const SearchInputBox = () => {
+interface SearchInputBoxProps {
+  onSearch: (searchText: string) => void;
+  resultsNotFound?: boolean;
+}
+
+export const SearchInputBox: FC<SearchInputBoxProps> = ({
+  onSearch,
+  resultsNotFound,
+}) => {
+  // states
+  const [searchKeyWord, setSearchKeyWord] = useState<string>('');
+  const handleSearch = () => {
+    onSearch(searchKeyWord);
+  };
   return (
     <div className='rounded-[0.93rem]  flex items-center relative w-full my-6 shadow-[0px_16px_30px_-10px_rgba(70,96,187,0.20)]'>
       <Image
@@ -17,9 +30,23 @@ export const SearchInputBox = () => {
       <Input
         type='text'
         placeholder='Search GitHub username…'
-        className='rounded-[0.95rem]  text-lg  pl-14 pr-20 h-[4rem] w-full'
+        className='rounded-[0.95rem]  text-lg  pl-14 pr-20 ring-0 h-[4rem] w-full border-none'
+        onChange={(e) => setSearchKeyWord(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onSearch((e.target as HTMLInputElement).value);
+          }
+        }}
       />
-      <Button className='absolute right-2 h-[3.12rem] bg-[#0079FF] rounded-r-[0.62rem] w-[6.6rem]'>
+      {resultsNotFound && (
+        <div className='absolute right-[8rem] px-2 text-[#F74646] font-bold'>
+          No results
+        </div>
+      )}
+      <Button
+        className='absolute right-2 h-[3.12rem] bg-[#0079FF] rounded-r-[0.62rem] w-[6.6rem]'
+        onClick={handleSearch}
+      >
         Search
       </Button>
     </div>
--- containers/github-container/github-container.tsx
+++ containers/github-container/github-container.tsx
@@ -1,13 +1,19 @@
+'use client';
+
 import React from 'react';
 import { ModeToggle } from '@/components/mode-toggler';
 
 import { SearchInputBox } from '@/components/search-input-box/search-input-box';
 export const GithubContainer = () => {
+  const handleSearch = (search: string) => {
+    console.log('search:', search);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center '>
       <ModeToggle />
       <div className='flex flex-col  w-[45rem]'>
-        <SearchInputBox />
+        <SearchInputBox onSearch={(data) => handleSearch(data)} />
 
         <div className=''>GithubContainer GithubContainer</div>
         <div className=''>GithubContainer GithubContainer</div>
